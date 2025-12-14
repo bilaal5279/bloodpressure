4a3d111808934a15e7d0b355ce1f2e8bd1ecd16d
bilaal5279 <bilaal5279@gmail.com>
2025-12-14 21:36:15 +0000

    Initial Commit

---
+++ bloodpressure/bloodpressureApp.swift
@@ -0,0 +1,32 @@
+//
+//  bloodpressureApp.swift
+//  bloodpressure
+//
+//  Created by Bilaal Ishtiaq on 14/12/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct bloodpressureApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
